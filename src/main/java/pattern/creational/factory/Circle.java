3a9dea46930cd97e13e494fcae71dc736c11c3fc
Thang Nguyen (Win) <nguyenhongthangnl@gmail.com>
2016-03-17 23:01:13 +0700

    Initialise

---
+++ src/main/java/pattern/creational/factory/Circle.java
@@ -0,0 +1,12 @@
+package pattern.creational.factory;
+
+/**
+ * Created by thangnguyen-pmac on 3/17/16.
+ */
+public class Circle implements IShape{
+
+
+    public void draw() {
+        System.out.println("Circle shape;");
+    }
+}
---
+++ src/main/java/pattern/creational/factory/IShape.java
@@ -0,0 +1,9 @@
+package pattern.creational.factory;
+
+/**
+ * Created by thangnguyen-pmac on 3/17/16.
+ */
+public interface IShape {
+
+    public void draw();
+}
